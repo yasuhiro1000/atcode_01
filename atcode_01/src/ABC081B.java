eb07b2619b1f9f771c63f235eb1c5d9c2c8ff751
yasuhiro <yasuhiro1000gmail.com>
2023-01-12 06:10:13 +0900

    テスト

--- atcode_01/src/ABC081B.java
+++ atcode_01/src/ABC081B.java
@@ -4,14 +4,29 @@ public static void main(String[] args) throws Exception {
     Scanner sca =new Scanner(System.in);
     int input_N = sca.nextInt();
     int[] arrayA = new int[input_N];
+    int count = 0;
+    int p = 0;
 //入力よ読み込み、配列収納
     for (int i = 0;i<input_N;i++){
         int input_A = sca.nextInt();
         arrayA[i] = input_A;  
         System.out.println(arrayA[i]);
     }
-//全部の偶数判定
-//全て２で割る
+//全部の偶数判定 
+    while ( p <arrayA.length+1){
+        if (arrayA[p]%2==1){
+            System.out.println(count);
+            System .exit(0);
+        }
+        arrayA[p]=arrayA[p]/2;
+        if (p>arrayA.length){
+                p=0;
+        }
+                p++;
+                count++;
     }
-    System.out.println(count);    
-    }
+                System.out.println(count);
+        }}
+
+
+    
