1fc8ce7bd87e06333539970e0e296afb97d4f1d1
yasuhiro <yasuhiro1000gmail.com>
2023-01-12 21:13:00 +0900

    vscode_index_sakujyo

--- atcode_01/src/ABC081B.java
+++ atcode_01/src/ABC081B.java
@@ -1,32 +1,25 @@
 import java.util.*; 
 public class ABC081B {
-public static void main(String[] args) throws Exception {
+public static void main(String[] args)  {
     Scanner sca =new Scanner(System.in);
     int input_N = sca.nextInt();
     int[] arrayA = new int[input_N];
-    int count = 0;
+    int count ;
     int p = 0;
-//入力よ読み込み、配列収納
     for (int i = 0;i<input_N;i++){
         int input_A = sca.nextInt();
-        arrayA[i] = input_A;  
-        System.out.println(arrayA[i]);
-    }
-//全部の偶数判定 
-    while ( p <arrayA.length+1){
-        if (arrayA[p]%2==1){
-            System.out.println(count);
-            System .exit(0);
-        }
-        arrayA[p]=arrayA[p]/2;
-        if (p>arrayA.length){
-                p=0;
-        }
-                p++;
+        count=0;
+        while(input_A>2){
+            if (input_A%2==1){
+                System.out.println("shuuryou"+ count);
+            }else {
+                input_A=input_A/2;
+                System.out.println(input_A);
                 count++;
-    }
-                System.out.println(count);
-        }}
-
-
+            }
+            }
+            System.out.println(count);
+            arrayA[i]=count;
+     }} }
+    
     
